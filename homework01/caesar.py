85977705ca6a8eafdc967f21dfa2e5c834b64cc8
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-10-11 22:25:33 +0300

    init

---
+++ homework01/caesar.py
@@ -0,0 +1,58 @@
+def encrypt_caesar(plaintext):
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+
+    # PUT YOUR CODE HERE
+    ciphertext = ''
+    for letter in plaintext:
+        if 'a' <= letter <= 'z' or 'A' <= letter <= 'Z':
+            new_letter = ''
+            if 'a' <= letter <= 'z':
+                new_letter = chr((ord(letter) + 3 - 97) % 26 + 97)
+            else:
+                new_letter = chr((ord(letter) + 3 - 65) % 26 + 65)
+        else:
+            new_letter = letter
+        ciphertext = ciphertext + new_letter
+
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext):
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+
+    # PUT YOUR CODE HERE
+    plaintext = ''
+    for letter in ciphertext:
+        if 'a' <= letter <= 'z' or 'A' <= letter <= 'Z':
+            new_letter = ''
+            if 'a' <= letter <= 'z':
+                new_letter = chr((ord(letter) - 3 - 97) % 26 + 97)
+            else:
+                new_letter = chr((ord(letter) - 3 - 65) % 26 + 65)
+        else:
+            new_letter = letter
+        plaintext = plaintext + new_letter
+
+    return plaintext
