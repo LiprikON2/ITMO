ec5052fea86f66cb7c9b5493b8f4b4c145cd8ff1
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-11-17 23:31:07 +0300

    checkpoint

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.locals import *
 import random
 import time
+# from sys import argv
+import argparse
+
 
 from life import GameOfLife
 from ui import UI
@@ -14,17 +17,17 @@ class GUI(UI):
         
         self.cell_size = cell_size
         
-        # px
+        # width and height in px
         self.width = self.life.cols * self.cell_size
         self.height = self.life.rows * self.cell_size
         
+        # in px
         self.screen_size = self.width, self.height
         
         self.screen = pygame.display.set_mode(self.screen_size)
         
         self.speed = speed
         
-        self.isPaused = False
 
     def draw_lines(self) -> None:
         """ Отрисовать сетку """
@@ -50,12 +53,21 @@ class GUI(UI):
                     pygame.draw.rect(self.screen, pygame.Color('White'),
                                      ((col * self.cell_size, row * self.cell_size),
                                       (self.cell_size, self.cell_size)))
+                    
     def pause(self) -> None:
-        self.isPaused = True
-        while self.isPaused:
+        """ Постановка игры на паузу """
+        
+        isPaused = True
+        self.draw_grid()
+        while isPaused:
             for event in pygame.event.get():
+                if event.type == QUIT:
+                    isPaused = False
+                    break
+                
+                # On keydown: if spacebar is pressed - RESUME
                 if event.type == pygame.KEYDOWN and event.key == 32:
-                    self.isPaused = False
+                    isPaused = False
                         
                 # On mouse button press: if right click - HANDLE CLICK
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
@@ -63,31 +75,29 @@ class GUI(UI):
             time.sleep(0.01)
             
     def handle_click(self, event):
+        """ Преключение состояния выбраной клетки """
+        
         x, y = event.pos
         target_row = y // self.cell_size
         target_col = x // self.cell_size
         
-        # get clicked cell
-        target = self.life.prev_generation[target_row][target_col]
-        print(target)
-        # kill it
-        if target:
-            target = 0
-        # or resurrect it
+        self.life.curr_generation = self.life.prev_generation.copy()
+    
+        # kill cell
+        if self.life.curr_generation[target_row][target_col]:
+            self.life.curr_generation[target_row][target_col] = 0
+        # or resurrect cell
         else:
-            target = 1
+            self.life.curr_generation[target_row][target_col] = 1
             
-        target = self.life.curr_generation
-        
         self.draw_grid()
         self.draw_lines()
-        
         pygame.display.flip()
             
-        print('Tar row:', target_row, "Tar col:", target_col)
                         
     def run(self) -> None:
         """ Запустить игру """
+        
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption('Game of Life')
@@ -95,7 +105,8 @@ class GUI(UI):
 
         
         running = True
-        while running and self.life.is_changing and not self.life.is_max_generations_exceed:
+        # while running and self.life.is_changing and not self.life.is_max_generations_exceed:
+        while running and not self.life.is_max_generations_exceed:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
@@ -103,22 +114,36 @@ class GUI(UI):
                 # On keydown: if spacebar is pressed - PAUSE
                 if event.type == pygame.KEYDOWN and event.key == 32:
                     self.pause()
+                    
             # Отрисовка списка клеток
             self.draw_grid()
+                    
             # Выполнение одного шага игры (обновление состояния ячеек)
             self.life.step()
             
             self.draw_lines()
             pygame.display.flip()
             
-            
-                        
-                
+            # Pause the game if nothing is changing
+            if not self.life.is_changing:
+                self.pause()
             clock.tick(self.speed)
+    
                     
-        pygame.quit()
 
 if __name__ == '__main__':
+    # width, height, cell_size = argv
+    # print(argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--width")
+    parser.add_argument("--height")
+    parser.add_argument("--cell_size")
+    
+    parser.parse_args()
+    
+    
+    
+    
     random.seed(12342)
-    game = GUI(GameOfLife((10, 10), True, 30), 40, 0.25)
+    game = GUI(GameOfLife((20, 20), True, 999), 40, 10)
     game.run()
