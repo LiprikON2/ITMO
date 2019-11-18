020750ad6510d9bffacbb2da15f34a4ff92cb5be
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-11-18 22:15:49 +0300

    Update bot.py

--- homework05/bot.py
+++ homework05/bot.py
@@ -1,7 +1,9 @@
 import requests
-import config
 import telebot
 from bs4 import BeautifulSoup
+import datetime
+
+import config
 
 
 # telebot.apihelper.proxy = {'https':'31.186.102.162:3128'}
@@ -41,6 +43,44 @@ def parse_schedule_for_a_monday(web_page):
 
     return times_list, locations_list, lessons_list
 
+
+def today(weekday):
+    hour = datetime.datetime.now().hour
+    minute = datetime.datetime.now().minute
+    week = datetime.date.today().isocalendar()[1]
+    weekday = datetime.datetime.today().weekday()
+    
+    
+    
+    id_attr = str(weekday + 1) + 'day'
+    return {'hour': hour, 'minute': minute, 'week': week, 'id_attr': id_attr}
+    # return {'hour': hour, 'minute': minute, 'week': week, 'weekday': weekday}
+    
+    
+    
+# MY CODE
+def parse_schedule(web_page, day):
+    soup = BeautifulSoup(web_page, "html5lib")
+    
+    # Получаем таблицу с расписанием на понедельник
+    schedule_table = soup.find("table", attrs={"id": day['id_attr']})
+    
+    # Время проведения занятий
+    times_list = schedule_table.find_all("td", attrs={"class": "time"})
+    times_list = [time.span.text for time in times_list]
+    
+    # Место проведения занятий
+    locations_list = schedule_table.find_all("td", attrs={"class": "room"})
+    locations_list = [room.span.text for room in locations_list]
+
+    # Название дисциплин и имена преподавателей
+    lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
+    lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
+    lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
+
+    return times_list, locations_list, lessons_list
+    
+
 @bot.message_handler(commands=['monday'])
 def get_monday(message):
     """ Получить расписание на понедельник """
@@ -57,15 +97,36 @@ def get_monday(message):
 @bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
 def get_schedule(message):
     """ Получить расписание на указанный день """
-    # PUT YOUR CODE HERE
-    pass
+
+    
+    weekday, group = message.text.split()
+    day = today(weekday)
+    web_page = get_page(group)
+    times_lst, locations_lst, lessons_lst = \
+        parse_schedule(web_page)
+    resp = ''
+    for time, location, lession in zip(times_lst, locations_lst, lessons_lst):
+        resp += '<b>{}</b>, {}, {}\n'.format(time, location, lession)
+    bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
 @bot.message_handler(commands=['near'])
 def get_near_lesson(message):
     """ Получить ближайшее занятие """
-    # PUT YOUR CODE HERE
-    pass
+
+    _, group = message.text.split()
+    web_page = get_page(group)
+    times_lst, locations_lst, lessons_lst = \
+        parse_schedule(web_page, day)
+    resp = ''
+    for time, location, lession in zip(times_lst, locations_lst, lessons_lst):
+        resp += '<b>{}</b>, {}, {}\n'.format(time, location, lession)
+    bot.send_message(message.chat.id, resp, parse_mode='HTML')
+
+
+
+
+
 
 
 @bot.message_handler(commands=['tommorow'])
@@ -83,5 +144,6 @@ def get_all_schedule(message):
 
 
 if __name__ == '__main__':
+    print(today())
     bot.polling(none_stop=True)
 
