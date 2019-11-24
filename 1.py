e7aaca3770a533fd7fb69caa665622a2058331d6
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-11-24 15:58:23 +0300

    Removed a bunch

--- 1.py
+++
@@ -1,6 +0,0 @@
-x = [int(i) for i in input().split()]
-sum = 0
-for i in range(len(x)):
-    sum += x[i]
-    
-print(sum)
--- hello.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if hasattr(sys, 'real_prefix'):
-    print("venv is online!")
-print("Hello world!")
-print(sys.real_prefix)
-print("Hello world!")
-   
