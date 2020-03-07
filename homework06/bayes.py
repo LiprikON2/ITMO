370569a0dd04835f9e116510489d864e9032e5d7
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2020-03-07 04:02:19 +0300

    Clusmy af but *kinda* done

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,14 +1,15 @@
 from db import News, session, engine
 from math import log
 from nltk.tokenize import RegexpTokenizer
+from pprint import pprint as pp
+from operator import itemgetter
 
 class NaiveBayesClassifier:
 
     def __init__(self, alpha=1):
         self.alpha = alpha
         self.labels = []
-        self.sanitized_titles = []
-        self.train = [] # title + label pair
+        self.classified_words = [] # list of word_info dicts
 
     def fit(self, X, y):
         """ Fit Naive Bayes classifier according to X, y. 
@@ -17,22 +18,20 @@ class NaiveBayesClassifier:
         """
         # List unique classes (labels)
         self.labels = list(set(y))
-        # Probability of a class (label)
-        self.label_probability = 1 / len(labels)
         
         tokenizer = RegexpTokenizer(r'\w+')
         
         words = []
+        sanitized_titles = []
         for title in X:
             # Remove punctuation and lowercase words
             sanitized_title = list(map(str.lower, tokenizer.tokenize(title)))
             
-            self.sanitized_titles.append(' '.join(sanitized_title))
+            sanitized_titles.append(' '.join(sanitized_title))
             words.extend(sanitized_title)
         
         unique_words = list(set(words))
-        
-        for word in unique_words[:100]:
+        for word in unique_words:
             word_info = {
                 'word': word,
                 'occur_in_class': [], # list of dicts
@@ -43,7 +42,7 @@ class NaiveBayesClassifier:
             # Dynamically count word occurences in classes (labels)
             for label in self.labels:
                 count = 0
-                for title_index, title in enumerate(self.sanitized_titles):
+                for title_index, title in enumerate(sanitized_titles):
                     if y[title_index] == label:
                         count += title.count(word)
                         
@@ -64,6 +63,7 @@ class NaiveBayesClassifier:
                     if list(occur.keys())[0] == label:
                         occur_in_class = occur[f'{label}']
                 
+                # Formula: https://i.imgur.com/oaym6LY.png 
                 prob = log((occur_in_class + self.alpha)/(total_count + self.alpha * len(unique_words)))
                 
                 # print(word, 'has', prob, 'probabilty of appearing in', label)
@@ -71,22 +71,64 @@ class NaiveBayesClassifier:
                     f'{label}': prob
                 })
             
-        
-        # print(words)
+            self.classified_words.append(word_info)
+        # pp(self.classified_words)
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
-        pass
-
+        tokenizer = RegexpTokenizer(r'\w+')
+        
+        predictions = []
+        for title in X:
+            # Remove punctuation and lowercase words
+            sanitized_title = list(map(str.lower, tokenizer.tokenize(title)))
+            
+            
+            prob_sums = []
+            for label in self.labels:
+                prob_sum = log(1 / len(labels))
+                
+                for word in sanitized_title:
+                    word_info = list(filter(lambda word_info: word_info['word'] == word, self.classified_words))
+                    
+                    # print(word, 'is in', word_info, 'dict')
+                    if word_info:
+                        word_info = word_info[0]
+                        for prob in word_info['prob_in_class']:
+                            if list(prob.keys())[0] == label:
+                                prob_in_class = prob[f'{label}']
+                                prob_sum += prob_in_class
+                prob_sums.append((label, prob_sum))
+            
+            
+            # ref: https://stackoverflow.com/questions/13145368/find-the-maximum-value-in-a-list-of-tuples-in-python
+            prediction = (title, max(prob_sums,key=itemgetter(1))[0], max(prob_sums,key=itemgetter(1))[1])
+            predictions.append(prediction)
+        return predictions
+            
+        
+            
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
-
+        success = 0
+        fail = 0
+        for i in range(len(X_test)):
+            print(X_test[i][1])
+            if X_test[i][1] == y_test[i]:
+                success += 1
+            else:
+                fail += 1
+        total = success + fail
+        accuracy = success / total
+        print(accuracy)
+        # X_test
+    
 if __name__ == '__main__':
     s = session()
     rows = s.query(News).filter(News.label != None).all()
     titles = [row.title for row in rows]
     labels = [row.label for row in rows]
     bayers = NaiveBayesClassifier()
-    bayers.fit(titles, labels)
-    
+    bayers.fit(titles[:700], labels[:700])
+    predictions = bayers.predict(titles[701:])
+    bayers.score(predictions, labels[701:])
