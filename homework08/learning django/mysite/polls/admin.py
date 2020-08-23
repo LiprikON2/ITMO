4fa91faf5192d8e17e2ed9336a121ec94dd9ed0e
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2020-08-23 17:26:34 +0300

    Tutorial part 7

--- homework08/learning django/mysite/polls/admin.py
+++ homework08/learning django/mysite/polls/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy
 
 from .models import *
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # Filter options: “Any date”, “Today”, “Past 7 days”, “This month”, “This year”.
+    list_filter = ['pub_date']
+    # Search capability
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+
+# Change admin template
+
+admin.site.site_title = 'Admin'
+admin.site.site_header = 'Administration'
+admin.site.index_title = 'Polls app'
--- homework08/learning django/mysite/polls/models.py
+++ homework08/learning django/mysite/polls/models.py
@@ -13,6 +13,12 @@ class Question(models.Model):
     
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    # Admin settings
+    # https://docs.djangoproject.com/en/3.1/_images/admin12t.png
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
