4b1ab612dea1b777c40215e1f28b69279e17705a
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2020-10-04 20:56:14 +0300

    cat_file() implemented

--- homework09/mygit.py
+++ homework09/mygit.py
@@ -24,24 +24,48 @@ def hash_object(file):
     content = file.read()
     # `\0` is a null character, used to separate header and content
     header = f'blob {len(content)}\0'
-    print(header)
     
-    store = header + content
-    hash_sum = hashlib.sha1(store.encode()).hexdigest()
+    blob = header + content
+    hash_sum = hashlib.sha1(blob.encode()).hexdigest()
     print(hash_sum)
     
-    foldername = hash_sum[:2]
-    filename = hash_sum[2:]
-    folder_path = f'.git/objects/{foldername}'
-    
-    try:
-        os.makedirs(folder_path)
-    except FileExistsError:
-        pass
+    if '-w' in sys.argv:
+        foldername = hash_sum[:2]
+        filename = hash_sum[2:]
+        folder_path = f'.git/objects/{foldername}'
         
-    with open(os.path.join(folder_path, filename), 'wb') as blob_file:
-        compressed_blob = zlib.compress(store.encode())
-        blob_file.write(compressed_blob)
+        os.makedirs(folder_path, exist_ok=True)
+            
+        with open(os.path.join(folder_path, filename), 'wb') as blob_file:
+            compressed_blob = zlib.compress(blob.encode())
+            blob_file.write(compressed_blob)
+
+def cat_file(hash_sum):
+    
+    if '-p' in sys.argv:
+        foldername = hash_sum[:2]
+        filename = hash_sum[2:]
+        folder_path = f'.git/objects/{foldername}'
+        
+        try:
+            with open(os.path.join(folder_path, filename), 'rb') as blob_file:
+                print(os.path.join(folder_path, filename))
+                blob = zlib.decompress(blob_file.read())
+                header_len = blob.find(b'\x00') + 1
+                
+                # blob 25\x00789c4bcac94f...
+                # ^^^^^^^^^^^
+                header = blob[:header_len]
+                # blob 25\x00789c4bcac94f...
+                #      ^^
+                content_len = int(header[blob.find(b' '):blob.find(b'\x00')].decode('ascii'))
+                # blob 25\x00789c4bcac94f...
+                #            ^^^^^^^^^^^^^^^
+                content = blob[header_len:header_len + content_len].decode('ascii')
+                
+                print(content)
+        except FileNotFoundError:
+            print(f'Not a valid object name {hash_sum}')
     
 def check_if_init():
     if not os.path.exists('.git'):
@@ -52,22 +76,25 @@ def main():
     command = sys.argv[1]
     if command == 'init':
         try:
-            os.makedirs('.git/objects')
-            os.makedirs('.git/refs')
-            # with open('.git/HEAD', 'r') as f:
-            #     f.write('ref: refs/heads')
+            os.makedirs('.git/objects', exist_ok=True)
+            os.makedirs('.git/refs/heads', exist_ok=True)
+            os.makedirs('.git/refs/tags', exist_ok=True)
+            with open('.git/HEAD', 'w+') as f:
+                f.write('ref: refs/heads')
             print('Initialized git directory')
         except FileExistsError:
             pass
         
     elif command == 'cat-file':
-        pass
+        check_if_init()
+        hash_sum = sys.argv[2]
+        cat_file(hash_sum)
     elif command == 'hash-object':
         check_if_init()
         file_path = sys.argv[2]
-        with open(file_path, 'r+') as f:
+        with open(file_path, 'r') as f:
             hash_object(f)
-            
+        
         
     elif command == 'is-tree':
         pass
